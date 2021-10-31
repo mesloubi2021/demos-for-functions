8cf1efceadc12df79324274e5e5169c5fed621d1
Hardik Sachan <sachanhardik@gmail.com>
2021-11-01 00:10:19 +0530

    fixed gradle configs and imports

--- kotlin/generate-bitly-url/src/main/kotlin/Main.kt
+++ kotlin/generate-bitly-url/src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
-import com.mashape.unirest.http.Unirest
-import com.mashape.unirest.http.exceptions.UnirestException
+import kong.unirest.Unirest
+import kong.unirest.UnirestException
 
 var API_KEY: String? = null
 
 @Throws(UnirestException::class)
-fun main(args: Array<String>) 
+fun main(args: Array<String>) {
     API_KEY = System.getenv("BITLY_API_KEY")
     val url = System.getenv("APPWRITE_FUNCTION_DATA")
 
     if (url != null && url.isNotEmpty()) {
-        try {         
+        try {
             val response = getBitlyLink(url)
             println(response)
         } catch (e: Exception) {
@@ -24,8 +24,8 @@ fun main(args: Array<String>)
 @Throws(UnirestException::class)
 fun getBitlyLink(url: String?): String {
     val request = Unirest.post("https://api-ssl.bitly.com/v4/shorten")
-        .header("Authorization", "Bearer " + API_KEY)
+        .header("Authorization", "Bearer $API_KEY")
         .field("long_url", url)
         .asJson()
-    return request.getBody().link
+    return request.body.toPrettyString()
 }
